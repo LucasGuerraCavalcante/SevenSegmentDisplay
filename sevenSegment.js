f555478610e54ddcd958e061b7d4acb3cccc7fa5
COPLE - CNPq <lucas.cavalcante@cnpq.br>
2019-07-24 10:09:44 -0300

    Adding basic files

---
+++ sevenSegment.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+    sevenSergment();
+}
+
+function sevenSergment() {
+    push();
+    fill(0);
+    rect(20, 100, 20, 45);
+
+
+    pop();
+}
