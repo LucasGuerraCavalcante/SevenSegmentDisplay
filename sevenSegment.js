8403dcdd472fcd5679e3382fb03885081d147b9c
COPLE - CNPq <lucas.cavalcante@cnpq.br>
2019-07-24 11:17:54 -0300

    Building the seven segment display

--- sevenSegment.js
+++ sevenSegment.js
@@ -4,14 +4,37 @@ function setup() {
 
 function draw() {
     background(220);
-    sevenSergment();
+    sevenSergment(0x7B);
 }
 
-function sevenSergment() {
-    push();
-    fill(0);
-    rect(20, 100, 20, 45);
+function getColor(val, shift) {
+    let r = 0;
+    let g = 255;
+    let b = 0;
+    let a = 255 * ((val >> shift) & 1);
+    return color(r,g,b,a)
+}
 
-
-    pop();
+function sevenSergment(val) {
+    // A Segment
+    fill(getColor(val, 6));
+    rect(60, 20, 78, 18); // (x, y, width, lenght)
+    // B Segment
+    fill(getColor(val, 5));
+    rect(140, 40, 18, 98);
+    // C Segment
+    fill(getColor(val, 4));
+    rect(140, 160, 18, 98);
+    // D Segment
+    fill(getColor(val, 3));
+    rect(60, 260, 78, 18);
+    // E Segment
+    fill(getColor(val, 2));
+    rect(40, 160, 18, 98);
+    // F Segment
+    fill(getColor(val, 1));
+    rect(40, 40, 18, 98);
+    // G Segment
+    fill(getColor(val, 0));
+    rect(60, 140, 78, 18);
 }
