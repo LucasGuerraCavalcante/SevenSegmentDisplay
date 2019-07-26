ecc87d925877e4201ca02610b3f524a0dd26cf6c
COPLE - CNPq <lucas.cavalcante@cnpq.br>
2019-07-26 12:01:58 -0300

    Improving JavaScript

--- sevenSegment.js
+++ sevenSegment.js
@@ -1,14 +1,97 @@
-function displayOff() {
-    clear();
-    index = 0
-    document.getElementById("display").inputMode = sevenSergment();
-}
 
+// List (Array) with all hexadecimal codes related to each number to display (0 to 9 in order)
 listNumbers = [0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B]
+// Inital index
 index = 0
 
+// Initial RGB code when load the page styles and the display (Light Green)
+let r = 0;
+let g = 255;
+let b = 0;
+
+// Function to change those colors
+function changeColor() {
+    // To see which color did the user selected
+    var opt = selColor.options[selColor.selectedIndex].text;
+    if (opt == "Red") {
+        r = 255;
+        g = 0;
+        b = 0;
+        newColor = document.getElementsByClassName("useDisplayColor")
+        newColor.style.color = "rgb(255, 0, 0);";
+    } else if (opt == "Blue") {
+        r = 0;
+        g = 0;
+        b = 255;
+        newColor = document.getElementsByClassName("useDisplayColor")
+        newColor.style.color = "rgb(0, 0, 255);";
+    } else {
+        r = 0;
+        g = 255;
+        b = 0;
+        newColor = document.getElementsByClassName("useDisplayColor")
+        newColor.style.color = "rgb(0, 255, 0);";
+    }
+    clear();
+    document.getElementById("display").inputMode = sevenSergment(listNumbers[index]);
+    return (r, g, b)
+}
+
+function getColor(val, shift, r, g, b) {
+    let a = 40 + 255 * ((val >> shift) & 1);
+    return color(r, g, b, a)
+}
+
+function setup() {
+    var cnv = createCanvas(203, 300);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 3;
+    cnv.position(x, y);
+}
+
+function sevenSergment(val) {
+    background(0);
+    noStroke();
+    noFill();
+    // A Segment
+    fill(getColor(val, 6, r, g, b));
+    rect(60, 20, 78, 18, 13); // (x, y, width, lenght, corner)
+    // B Segment
+    fill(getColor(val, 5, r, g, b));
+    rect(140, 40, 18, 98, 13);
+    // C Segment
+    fill(getColor(val, 4, r, g, b));
+    rect(140, 160, 18, 98, 13);
+    // D Segment
+    fill(getColor(val, 3, r, g, b));
+    rect(60, 260, 78, 18, 13);
+    // E Segment
+    fill(getColor(val, 2, r, g, b));
+    rect(40, 160, 18, 98, 13);
+    // F Segment
+    fill(getColor(val, 1, r, g, b));
+    rect(40, 40, 18, 98, 13);
+    // G Segment
+    fill(getColor(val, 0, r, g, b));
+    rect(60, 140, 78, 18, 13);
+    // DP Segment
+    fill(getColor(val, 8, r, g, b));
+    rect(170, 260, 18, 18, 13);
+    // Display borders
+    fill(111);
+    rect(0, 0, 5, 300);
+    fill(111);
+    rect(0, 0, 300, 5);
+    fill(111);
+    rect(198, 0, 5, 300);
+    fill(111);
+    rect(0, 295, 300, 5);
+}
+
+// Function to the display the selected number
 function displayNumber() {
     clear();
+    // To see which number did the user selected
     var opt = selNumber.options[selNumber.selectedIndex].text;
     if (opt == "0") {
         index = 0
@@ -43,83 +126,21 @@ function displayNumber() {
     }
 }
 
+// Function to display the next number on order when you click the button
 function countNumbers() {
+    // To clean the draw
     clear();
+    // To turn the current index value into the next index value (+1)
     index = (index + 1) % listNumbers.length
     document.getElementById("display").inputMode = sevenSergment(listNumbers[index]);
 }
 
-function setup() {
-    var cnv = createCanvas(203, 300);
-    var x = (windowWidth - width) / 2;
-    var y = (windowHeight - height) / 3;
-    cnv.position(x, y);
-}
-
-let r = 0;
-let g = 255;
-let b = 0;
-
-function changeColor() {
-    var opt = selColor.options[selColor.selectedIndex].text;
-    if (opt == "Red") {
-        r = 255;
-        g = 0;
-        b = 0;
-    } else if (opt == "Blue") {
-        r = 0;
-        g = 0;
-        b = 255;
-    } else {
-        r = 0;
-        g = 255;
-        b = 0;
-    }
+// Function to turn the Display "off"
+function displayOff() {
+    // To clean the draw
     clear();
-    document.getElementById("display").inputMode = sevenSergment(listNumbers[index]);
-    return (r, g, b)
-}
-
-function getColor(val, shift, r, g, b) {
-    let a = 40 + 255 * ((val >> shift) & 1);
-    return color(r, g, b, a)
-}
-
-function sevenSergment(val) {
-    background(0);
-    noStroke();
-    noFill();
-    // A Segment
-    fill(getColor(val, 6, r, g, b));
-    rect(60, 20, 78, 18, 13); // (x, y, width, lenght, corner)
-    // B Segment
-    fill(getColor(val, 5, r, g, b));
-    rect(140, 40, 18, 98, 13);
-    // C Segment
-    fill(getColor(val, 4, r, g, b));
-    rect(140, 160, 18, 98, 13);
-    // D Segment
-    fill(getColor(val, 3, r, g, b));
-    rect(60, 260, 78, 18, 13);
-    // E Segment
-    fill(getColor(val, 2, r, g, b));
-    rect(40, 160, 18, 98, 13);
-    // F Segment
-    fill(getColor(val, 1, r, g, b));
-    rect(40, 40, 18, 98, 13);
-    // G Segment
-    fill(getColor(val, 0, r, g, b));
-    rect(60, 140, 78, 18, 13);
-    // DP Segment
-    fill(getColor(val, 8, r, g, b));
-    rect(170, 260, 18, 18, 13);
-    // Borders
-    fill(111);
-    rect(0, 0, 5, 300);
-    fill(111);
-    rect(0, 0, 300, 5);
-    fill(111);
-    rect(198, 0, 5, 300);
-    fill(111);
-    rect(0, 295, 300, 5);
+    // To reset the listNumbers Index
+    index = 0
+    // To draw it turned "off"
+    document.getElementById("display").inputMode = sevenSergment();
 }
